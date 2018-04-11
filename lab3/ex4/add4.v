253e67ed50b9cc4f6b9ec94649923cf483da96ad
Nitzan Carmi <nitzancarmi@archlab5.eng.tau.ac.il>
2018-04-11 15:40:00 +0300

    Ex4 finished
    Signed-off-by: Nitzan Carmi <nitzancarmi@archlab5.eng.tau.ac.il>

--- lab3/ex4/add4.v
+++ lab3/ex4/add4.v
@@ -1,9 +1,17 @@
-module  add4( sum, co, a, b, ci);
+`include "../ex3/fulladder.v"
+
+module add4(a, b, ci, sum, co);
 
   input   [3:0] a, b;
   input   ci;
   output  [3:0] sum;
   output  co;
+  wire     [3:0] citmp;
+  
+  assign citmp[0] = ci;
+  fulladder fa0(a[0], b[0], citmp[0], sum[0], citmp[1]);
+  fulladder fa1(a[1], b[1], citmp[1], sum[1], citmp[2]);
+  fulladder fa2(a[2], b[2], citmp[2], sum[2], citmp[3]);
+  fulladder fa3(a[3], b[3], citmp[3], sum[3], co);
 
-  // FILL HERE
 endmodule
