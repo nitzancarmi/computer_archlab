d17d857c3a40eb63f7e8b948136c82e4b070a004
Nitzan Carmi <nitzancarmi@archlab5.eng.tau.ac.il>
2018-04-13 11:39:52 +0300

    Ex7 module only
    Signed-off-by: Nitzan Carmi <nitzancarmi@archlab5.eng.tau.ac.il>

--- lab3/ex7/sat_coun.v
+++ lab3/ex7/sat_coun.v
@@ -1,7 +1,31 @@
 module sat_count(clk, reset, branch, taken, prediction);
    parameter N=2;
+   parameter counter_o[N-1:0];
+   parameter counter_n[N-1:0];
+
    input clk, reset, branch, taken;
    output prediction;
 
-   // FILL HERE
+   addsub add1(.result(counter_n),
+	       .operand_a(counter_o),
+	       .operand_b(1),
+               .mode(~taken));
+
+   always @(posedge clk)
+     begin
+	if (reset)
+	  counter <= 0;
+	else begin
+          if (~branch)
+            counter_o <= counter_o;
+          else
+	    case (taken)
+               0: counter_o <= (counter_o == 0) ? 0 : counter_n;
+               1: counter_o <= (counter_o == -1) ? -1 : counter_n;
+               default: counter_o <= counter_n;
+	    endcase
+        end
+     end
+
+    assign prediction = counter_o[N-1];
 endmodule
