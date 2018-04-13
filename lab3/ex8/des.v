75c7fa280bb67197d3d8a79448016af2de527991
Nitzan Carmi <nitzancarmi@archlab5.eng.tau.ac.il>
2018-04-13 16:26:04 +0300

    Ex8 Finished
    Signed-off-by: Nitzan Carmi <nitzancarmi@archlab5.eng.tau.ac.il>

--- lab3/ex8/des.v
+++ lab3/ex8/des.v
@@ -4,6 +4,28 @@ module des(clk, in, reset, out);
    
    input clk, in, reset;
    output [WIDTH-1:0] out;
-   
-   // Fill Here
+   reg [WIDTH-1:0] out_next = 0;
+   reg [WIDTH-1:0] _out = 0;
+   reg [WIDTH-2:0] timer = 1;
+
+   always @(posedge clk)
+     begin
+        out_next <= (out_next << 1) | in;
+//        $display("next out %b (in %b)", out_next, in);
+        timer <= timer << 1;
+
+	if (reset)
+	  _out <= 0;
+	else
+           _out <= (timer == 0) ? out_next : out;
+
+        if (timer == 0) begin
+//	   $display("assign new out: %b", out_next);
+           timer <= 1;
+        end
+
+     end
+
+     assign out = _out;
+
 endmodule
--- lab3/ex8/des_tb.v
+++ lab3/ex8/des_tb.v
@@ -1,11 +1,64 @@
 module main;
-   reg in, clk, reset;
+   reg in, clk, reset, correct;
    reg [7:0] buff;
    wire [7:0] out;
 
-   des uut (clk, in, reset, out);
+   des #(8) uut (clk, in, reset, out);
 
    always #5 clk = ~clk;
 
-   // Fill Here
+   initial
+     begin
+        #1;
+        $dumpfile("waves.vcd");
+        $dumpvars;
+        correct = 1;
+        clk = 0;
+        reset = 0;
+
+        in = 1;
+	#40;
+        in = 0;
+	#40;
+        #10;
+	//$display("out %d", out);
+        correct = correct & (out == 8'b01111000);
+        if (correct)
+		$display("simple test works");
+        in = 1;
+
+        //#2
+	#10;
+	//$display("out %d", out);
+	#10;
+	//$display("out %d", out);
+	#10;
+	reset = 1;
+	//$display("out %d", out);
+	#10;
+        correct = correct & (out == 8'b00000000);
+        if (correct)
+		$display("reset works");
+        reset = 0;
+
+	//$display("out %d", out);
+	#10;
+	//$display("out %d", out);
+	#10;
+	//$display("out %d", out);
+	#10;
+	//$display("out %d", out);
+	#10;
+	//$display("out %d", out);
+	#10;
+        correct = correct & (out == 8'b00111111);
+        if (correct)
+		$display("after reset works");
+	//$display("out %d", out);
+
+
+	if (correct)
+	  $display("all tests PASSED!");
+        $finish;
+     end // initial begin
 endmodule
