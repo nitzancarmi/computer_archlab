bf48e47eac9b58dbc3597141d62e62c025458355
Nitzan Carmi <nitzancarmi@archlab5.eng.tau.ac.il>
2018-05-13 11:27:04 +0300

    Add first version of lab5
    Signed-off-by: Nitzan Carmi <nitzancarmi@archlab5.eng.tau.ac.il>

--- lab5/sp.c
+++ lab5/sp.c
@@ -20,6 +20,9 @@ FILE *inst_trace_fp = NULL, *cycle_trace_fp = NULL;
 typedef struct sp_registers_s {
 	// 6 32 bit registers (r[0], r[1] don't exist)
 	int r[8];
+	int r_busy[8];
+
+	int jtaken;
 
 	// 32 bit cycle counter
 	int cycle_counter;
@@ -136,11 +139,25 @@ static void dump_sram(sp_t *sp, char *name, llsim_memory_t *sram)
 	fclose(fp);
 }
 
+static int is_jump_inst(int opcode) {
+	switch(opcode) {
+	case JLT:
+	case JLE:
+	case JEQ:
+	case JNE:
+	case JIN:
+		return true;
+	default:
+		return false;
+	}
+}
+
 static void sp_ctl(sp_t *sp)
 {
 	sp_registers_t *spro = sp->spro;
 	sp_registers_t *sprn = sp->sprn;
 	int i;
+	int opcode, dst, src0, src1;
 
 	fprintf(cycle_trace_fp, "cycle %d\n", spro->cycle_counter);
 	fprintf(cycle_trace_fp, "cycle_counter %08x\n", spro->cycle_counter);
@@ -207,25 +224,219 @@ static void sp_ctl(sp_t *sp)
 	// fetch0
 	sprn->fetch1_active = 0;
 	if (spro->fetch0_active) {
+		llsim_mem_read(sp->srami, spro->pc);
+
+		sprn->fetch1_active = 1;
+		sprn->fetch1_pc = spro->fetch0_pc;
 	}
 
 	// fetch1
-	// FILL HERE
+	sprn->dec0_active = 0;
+	if (spro->fetch1_active) {
+		sprn->dec0_inst = llsim_mem_extract_dataout(sp->srami, 31, 0);
+
+		sprn->dec0_active = 1;
+		sprn->dec0_pc = spro->fetch1_pc;
+	}
 	
 	// dec0
-	// FILL HERE
+	sprn->dec1_active = 0;
+	if (spro->dec0_active) {
+		opcode = spro->dec0_inst >> 25;
+		dst = (spro->dec0_inst >> 22) & 7;
+		src0 = (spro->dec0_inst >> 19) & 7;
+		src1 = (spro->dec0_inst >> 16) & 7;
 
+		sprn->dec1_pc = (is_jump_inst(opcode) & spro->jtaken) ? spro->dec0_immediate : spro->dec0_pc + 1;
+		sprn->dec1_opcode = opcode;
+		if (spro->r_busy[src0] || spro->r_busy[src1]) {
+			sprn->fetch0_active = 0;
+			sprn->fetch1_active = 0;
+			sprn->dec0_active = 1;
+		} else {
+			sprn->dec1_dst = dst;
+			sprn->dec1_src0 = src0;
+			sprn->dec1_src1 = src1;
+			sprn->dec1_immediate = spro->dec0_inst & 0xffff;
+			sprn->r_busy[dst] = 1;
+
+			sprn->fetch0_active = 1;
+			sprn->fetch1_active = 1;
+			sprn->dec0_active = 1;
+			sprn->dec1_active = 1;
+
+			sprn->dec1_pc = spro->dec0_pc;
+			sprn->dec1_inst = spro->dec0_inst;
+		}
+	}
+	
 	// dec1
-	// FILL HERE
+	sprn->exec0_active = 0;
+	if (spro->dec1_active) {
+		sprn->r[1] = spro->dec1_immediate;
+		sprn->exec0_alu0 = (spro->dec1_src0 == 1) ? spro->dec1_immediate : spro->r[spro->dec1_src0];
+		sprn->exec0_alu1 = (spro->dec1_src1 == 1) ? spro->dec1_immediate : spro->r[spro->dec1_src1];
+		if (spro->dec1_opcode == LHI)
+			sprn->exec0_alu0 = spro->dec1_dst;
 
+		sprn->exec0_active = 1;
+		sprn->exec0_pc = spro->dec1_pc;
+		sprn->exec0_inst = spro->dec1_inst;
+		sprn->exec0_opcode = spro->dec1_opcode;
+		sprn->exec0_dst = spro->dec1_dst;
+		sprn->exec0_src0 = spro->dec1_src0;
+		sprn->exec0_src1 = spro->dec1_src1;
+		sprn->exec0_immediate = spro->dec1_immediate;
+	}
+	
 	// exec0
-	// FILL HERE
+	sprn->exec1_active = 0;
+	if (spro->exec0_active) {
 
+		switch(spro->exec0_opcode) {
+		case ADD:
+			sprn->exec1_aluout = spro->exec0_alu0 + spro->exec0_alu1;
+			break;
+		case SUB:
+			sprn->exec1_aluout = spro->exec0_alu0 - spro->exec0_alu1;
+			break;
+		case LSF:
+			sprn->exec1_aluout = spro->exec0_alu0 << spro->exec0_alu1;
+			break;
+		case RSF:
+			sprn->exec1_aluout = spro->exec0_alu0 >> spro->exec0_alu1;
+			break;
+		case AND:
+			sprn->exec1_aluout = spro->exec0_alu0 & spro->exec0_alu1;
+			break;
+		case OR:
+			sprn->exec1_aluout = spro->exec0_alu0 | spro->exec0_alu1;
+			break;
+		case XOR:
+			sprn->exec1_aluout = spro->exec0_alu0 ^ spro->exec0_alu1;
+			break;
+		case LHI:
+			sprn->exec1_aluout = (spro->exec0_immediate << 16) + (spro->exec0_alu0 && 0xffff);
+			break;
+
+		case LD:
+			llsim_mem_read(sp->sramd, spro->exec0_alu1);
+			break;
+		case ST:
+			llsim_mem_set_datain(sp->sramd, spro->exec0_alu0, 31, 0);
+			llsim_mem_write(sp->sramd, spro->exec0_alu1);
+			break;
+
+		case JLT:
+			sprn->exec1_aluout = (spro->exec0_alu0 < spro->exec0_alu1);
+			break;
+		case JLE:
+			sprn->exec1_aluout = (spro->exec0_alu0 <= spro->exec0_alu1);
+			break;
+		case JEQ:
+			sprn->exec1_aluout = (spro->exec0_alu0 == spro->exec0_alu1);
+			break;
+		case JNE:
+			sprn->exec1_aluout = (spro->exec0_alu0 != spro->exec0_alu1);
+			break;
+		case JIN:
+			sprn->exec1_aluout = 1;
+			break;
+/*
+		case DMA:
+			if (spro->dma_state == DMA_STATE_IDLE) {
+				sprn->dma_raddr = spro->alu0;
+				sprn->dma_waddr = spro->alu1;
+				sprn->dma_cnt = spro->immediate;
+				sprn->dma_state = DMA_STATE_READ;
+			}
+			break;
+		case POL:
+			break;
+*/
+		case HLT:
+			break;
+		default:
+			printf("Unknown opcode %d\n", spro->opcode);
+		}
+
+		sprn->exec1_active = 1;
+		sprn->exec1_pc = spro->exec0_pc;
+		sprn->exec1_inst = spro->exec0_inst;
+		sprn->exec1_opcode = spro->exec0_opcode;
+		sprn->exec1_dst = spro->exec0_dst;
+		sprn->exec1_src0 = spro->exec0_src0;
+		sprn->exec1_src1 = spro->exec0_src1;
+		sprn->exec1_immediate = spro->exec0_immediate;
+		sprn->exec1_alu0 = spro->exec0_alu0;
+		sprn->exec1_alu1 = spro->exec0_alu1;
+	}
+	
 	// exec1
 	if (spro->exec1_active) {
+
+		switch(spro->opcode) {
+		case ADD:
+		case SUB:
+		case LSF:
+		case RSF:
+		case AND:
+		case OR:
+		case XOR:
+		case LHI:
+			sprn->r[spro->exec1_dst] = spro->exec1_aluout;
+			sprn->r_busy[spro->exec1_dst] = 0;
+			break;
+
+		case LD:
+			sprn->r[spro->exec1_dst] = llsim_mem_extract_dataout(sp->sramd, 31, 0);
+			sprn->r_busy[spro->exec1_dst] = 0;
+			break;
+		case ST:
+			break;
+
+		case JLT:
+		case JLE:
+		case JEQ:
+		case JNE:
+		case JIN:
+			if (spro->exec1_aluout) {
+				sprn->r[7] = spro->pc - 1;
+			}
+			if (spro->exec1_aluout != spro->jtaken) {
+				sprn->fetch0_active = 1;
+				sprn->fetch1_active = 0;
+				sprn->dec0_active = 0;
+				sprn->dec1_active = 0;
+				sprn->exec0_active = 0;
+				sprn->exec1_active = 0;
+				sprn->pc = spro->exec1_aluout ? spro->immediate : spro->exec1_pc + 1;
+			}
+
+			sprn->jtaken = spro->exec1_aluout;
+			break;
+
+		case DMA:
+			break;
+		case POL:
+			sprn->r[spro->dst] = spro->dma_cnt;
+		case HLT:
+			break;
+		default:
+			printf("Unknown opcode %d\n", spro->opcode);
+		}
+
+
+
 		// FILL HERE
 		if (spro->exec1_opcode == HLT) {
 			llsim_stop();
+			sprn->fetch0_active = 0;
+			sprn->fetch1_active = 0;
+			sprn->dec0_active = 0;
+			sprn->dec1_active = 0;
+			sprn->exec0_active = 0;
+			sprn->exec1_active = 0;
 			dump_sram(sp, "srami_out.txt", sp->srami);
 			dump_sram(sp, "sramd_out.txt", sp->sramd);
 		}
